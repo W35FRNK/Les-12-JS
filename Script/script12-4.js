3a8041db10a12bb190709ce9b3b2d1fca4854c41
Frank Wesdijk <frank.wesdijk@gmail.com>
2023-06-12 14:06:44 +0200

    fout check added

--- Script/script12-4.js
+++ Script/script12-4.js
@@ -4,14 +4,21 @@ $(document).ready(function () {
 		{ app_id: "0c9762d4977945c0b87e91a20a2a908c" },
 		function (data) {
 			const koersEur = data.rates.EUR;
+			console.log(data.rates);
 			console.log(koersEur);
 
 			$("#btnValuta").on("click", function () {
 				const nrVal = parseInt($("#nrValuta").val());
 				console.log(nrVal);
+				var check = $.isNumeric(nrVal);
 				console.log(nrVal * koersEur);
 				var usdEur = nrVal * koersEur;
-				$("#divResult").append("€ " + usdEur);
+
+				if (check) {
+					$("#divResult").append("€ " + usdEur);
+				} else {
+					alert("Voer een getal in!");
+				}
 			});
 		}
 	);
